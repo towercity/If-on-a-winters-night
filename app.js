4bcc6e22c33e42a8c237987e9e84f2e85ecb0ab1
mnerger <matthewjnerger3@gmail.com>
2017-03-28 15:39:50 -0400

    Prepare for heroku

--- app.js
+++ app.js
@@ -12,33 +12,50 @@ const fs = require("fs");
 const Sequelize = require('sequelize');
 const Fetch = require("node-fetch");
 const FormData = require("form-data");
+const pg = require('pg');
+
 
 const server = new Hapi.Server({
-	connections: {
-		routes: {
-			files: {
-				relativeTo: Path.join(__dirname, 'public')
-			}
-		}
-	}
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
 });
+
+
+var sequelize;
+
 
 server.connection({
-	port: 3000
+    port: (process.env.PORT || 3000)
 });
 
-var sequelize = new Sequelize('db', 'username', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
 
-	pool: {
-		max: 5,
-		min: 0,
-		idle: 10000
-	},
+if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        logging: true //false
+    })
+} else {
+    sequelize = new Sequelize('db', 'username', 'password', {
+        host: 'localhost',
+        dialect: 'sqlite',
 
-	storage: 'db.sqlite'
-});
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
+
+        // SQLite only
+        storage: 'db.sqlite'
+    });
+}
 
 var Text = sequelize.define('text', {
 	textName: {
